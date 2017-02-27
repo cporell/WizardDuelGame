2865b7be0a0f039d51d3f401ebc1c9da63bd5c41
cporell <cgporell@wpi.edu>
2017-02-26 22:21:28 -0500

    first commit

---
+++ Ravenwood Duel/Assets/Code/Core/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Game Manager started!!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 250, 100), "Game Manager started!!");
+        GUI.Label(new Rect(0, 20, 250, 100), "Time is" + System.DateTime.Now.ToString());
+    }
+}
